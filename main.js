7faa750745680a973bd8df566acf18b6f4314503
cinnamon-user <andreankhata@gmail.com>
2019-07-03 21:28:17 +0200

    Array Method Practice in JS

---
+++ main.js
@@ -0,0 +1,15 @@
+// My first array methods in application.
+let contents = [1,2,3,4,5,6,7,8,9,0];
+
+const result = contents.find((content)=>{
+    return content >= 5;
+});
+console.log(result);
+
+// forEach method
+const answer = contents.find((content)=>{
+    return content * 2;
+});
+
+
+
